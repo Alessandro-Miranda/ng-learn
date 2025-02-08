5333c4f5b746a7d07c468f8965add362af8e48e3
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-02-08 11:32:42 -0300

    Starting first project

--- apps/social-links-profile/jest.config.ts
+++ apps/social-links-profile/jest.config.ts
@@ -1,8 +1,8 @@
 export default {
-  displayName: 'ng-learn',
+  displayName: 'social-links-profile',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/apps/ng-learn',
+  coverageDirectory: '../../coverage/apps/social-links-profile',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
