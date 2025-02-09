6a4289913243c28a321f35891251b08f3325d117
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-02-09 11:38:37 -0300

    Create user and link dummy data

--- apps/social-links-profile/src/app/app.component.ts
+++ apps/social-links-profile/src/app/app.component.ts
@@ -1,11 +1,29 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { LinkData } from './models/socialLinks.models';
+import { UserData } from './models/user.model';
+import { SocialLinkComponent } from "./social-link/social-link.component";
+import { UserComponent } from "./user/user.component";
 
 @Component({
-  imports: [],
+  imports: [UserComponent, SocialLinkComponent, NgFor],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'social-links-profile';
+  user: UserData = {
+    name: 'Jessica Randall',
+    location: 'London, United Kingdom',
+    profileImage: '/assets/images/avatar-jessica.jpeg',
+    summary: 'Front-end developer and avid reader.'
+  }
+
+  socialLinks: LinkData[] = [
+    { label: 'GitHub', link: '' },
+    { label: 'Frontend Mentor', link: '' },
+    { label: 'LinkedIn', link: '' },
+    { label: 'Twitter', link: '' },
+    { label: 'Instagram', link: '' }
+  ]
 }
