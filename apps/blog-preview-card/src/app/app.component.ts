23cc87eafb87bbefd69dfdb30a169e73e933d38f
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-02-10 17:30:28 -0300

    Add dummy data to create the post card

--- apps/blog-preview-card/src/app/app.component.ts
+++ apps/blog-preview-card/src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { AuthorData } from './author/author.model';
+import { CardComponent } from "./card/card.component";
 
 @Component({
-  imports: [],
+  imports: [CardComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'blog-preview-card';
+  postCategory = ['Learning'];
+  publishedAt = '2023-12-21';
+  postTitle = 'HTML & CSS foundations';
+  postSummary =
+    'These languages are the backbone of every website, defining structure, content, and presentation.';
+  author: AuthorData = {
+    name: 'Greg Hooper',
+    profileImage: '/userpic.png'
+  };
+  postImage = '/post-image.png'
 }
