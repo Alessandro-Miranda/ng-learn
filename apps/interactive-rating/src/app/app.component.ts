0196028aa5f4b2d4d54b0a16c8a9ed52ecb65e96
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-02-11 15:05:24 -0300

    Add card component to app

--- apps/interactive-rating/src/app/app.component.ts
+++ apps/interactive-rating/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { CardComponent } from "./card/card.component";
 
 @Component({
-  imports: [],
+  imports: [CardComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'interactive-rating';
 }
