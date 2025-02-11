ee02e399ebfb1a646ea7032c096ab5cad109eb44
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-02-11 15:20:15 -0300

    Implement output and send event

--- apps/interactive-rating/src/app/rating-form/rating-form.component.ts
+++ apps/interactive-rating/src/app/rating-form/rating-form.component.ts
@@ -1,10 +1,20 @@
 import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-rating-form',
-  imports: [NgFor],
+  imports: [NgFor, FormsModule],
   templateUrl: './rating-form.component.html',
   styleUrl: './rating-form.component.css',
 })
-export class RatingFormComponent {}
+export class RatingFormComponent {
+  @Output() sendSelectedRate = new EventEmitter<string>();
+  selectedRate: string | undefined;
+
+  onSendRating() {
+    if (!this.selectedRate) alert('Please, select a rating');
+    
+    this.sendSelectedRate.emit(this.selectedRate);
+  }
+}
