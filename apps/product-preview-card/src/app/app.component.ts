87ec832c8260b6306a0abe2561295fb1f00be646
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-02-22 11:49:21 -0300

    Mock product data

--- apps/product-preview-card/src/app/app.component.ts
+++ apps/product-preview-card/src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CardComponent } from "./card/card.component";
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, CardComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'product-preview-card';
+  productInfo = {
+    image: {
+      src_desktop: 'images/image-product-desktop.jpg',
+      src_mobile: 'images/image-product-mobile.jpg',
+      alt: 'Gabrielle Essence Eau de Parfum',
+    },
+    category: '',
+    title: 'Gabrielle Essence Eau de Parfum',
+    summary: 'A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL',
+    price: {
+      actual: 149.99,
+      old: 169.99,
+    },
+  };
 }
