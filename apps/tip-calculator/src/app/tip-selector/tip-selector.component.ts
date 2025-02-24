c4165b5b6e8aebd32d50c77e2381e70935e26577
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-02-24 20:29:08 -0300

    Start component creation

---
+++ apps/tip-calculator/src/app/tip-selector/tip-selector.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tip-selector',
+  imports: [],
+  templateUrl: './tip-selector.component.html',
+  styleUrl: './tip-selector.component.css',
+})
+export class TipSelectorComponent {}
