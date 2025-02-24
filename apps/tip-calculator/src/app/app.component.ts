bde9cf32eb7f41ba8b86a3cb9eed9c84e0a7b413
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-02-24 20:29:51 -0300

    Import components

--- apps/tip-calculator/src/app/app.component.ts
+++ apps/tip-calculator/src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { BillInputComponent } from "./bill-input/bill-input.component";
+import { PeopleInputComponent } from "./people-input/people-input.component";
+import { TipSelectorComponent } from "./tip-selector/tip-selector.component";
+import { TipSummaryComponent } from "./tip-summary/tip-summary.component";
 
 @Component({
-  imports: [],
+  imports: [BillInputComponent, TipSelectorComponent, TipSummaryComponent, PeopleInputComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
