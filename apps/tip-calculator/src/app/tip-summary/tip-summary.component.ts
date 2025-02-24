4c2e526144e93cd7863c530b7f46063c6476a51e
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-02-24 20:13:44 -0300

    Create summary component and finish component style

---
+++ apps/tip-calculator/src/app/tip-summary/tip-summary.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tip-summary',
+  imports: [],
+  templateUrl: './tip-summary.component.html',
+  styleUrl: './tip-summary.component.css',
+})
+export class TipSummaryComponent {}
