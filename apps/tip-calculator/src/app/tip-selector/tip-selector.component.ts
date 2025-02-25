5055a977cf1b3ca6f6c95cb38f7a71051708d19e
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2025-02-24 22:41:55 -0300

    Finish component style

--- apps/tip-calculator/src/app/tip-selector/tip-selector.component.ts
+++ apps/tip-calculator/src/app/tip-selector/tip-selector.component.ts
@@ -1,9 +1,12 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tip-selector',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './tip-selector.component.html',
   styleUrl: './tip-selector.component.css',
 })
-export class TipSelectorComponent {}
+export class TipSelectorComponent {
+  defaultTips = [5, 10, 15, 25, 50];
+}
